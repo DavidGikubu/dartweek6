7a104c986783c12510d68cfed645647f7d472db1
Gikubu <davidndungu8203@gmail.com>
2024-03-28 11:22:03 +0300

    we create a program to count from 1 to 10 using a for loop.

--- bin/myprograms.dart
+++ bin/myprograms.dart
@@ -3,4 +3,11 @@ int sum(int a, int b) {
   return a + b;
 }
 
+//task 2:a program that prints numbers from 1 to 10 using a for loop.
+void printNumbers() {
+  for (int i = 1; i <= 10; i++) {
+    print(i);
+  }
+}
+
 void main(List<String> arguments) {}
